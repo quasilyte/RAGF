e722f8db543bd3ad215a27fde6e749242d11b4f4
quasilyte <quasilyte@gmail.com>
2016-09-23 11:21:00 +0300

    named GPR constants

--- src/core/arch/x86_64/constants.hpp
+++ src/core/arch/x86_64/constants.hpp
@@ -1,13 +1,24 @@
 #pragma once
 
 #include <core/typedefs.hpp>
+#include <core/arch/x86_64/gpr.hpp>
 
-//                     ----WRXB
-const byte REX     = 0b01000000;
-const byte REX_W   = 0b01001000;
-const byte REX_B   = 0b01000001;
-const byte REX_WB  = 0b01001001;
-const byte REX_WRB = 0b01001101;
+//                     ----WRXB     hex
+const byte REX     = 0b01000000; // 40
+const byte REX_W   = 0b01001000; // 48
+const byte REX_B   = 0b01000001; // 41
+const byte REX_WB  = 0b01001001; // 49
+const byte REX_WRB = 0b01001101; // 4d
+const byte REX_WR  = 0b01001100; // 4c
+
+const Gpr rax{0};
+const Gpr rcx{1};
+const Gpr rdx{2};
+const Gpr rbx{3};
+const Gpr rsp{4};
+const Gpr rbp{5};
+const Gpr rsi{6};
+const Gpr rdi{7};
 
 struct Mod {
   static const u64 SIB = 0;
