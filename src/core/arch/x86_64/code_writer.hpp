6a22141cb1fac3563825cd2192ee000bfeb4d9e2
quasilyte <quasilyte@gmail.com>
2016-09-24 23:56:31 +0300

    tidy up header

--- src/core/arch/x86_64/code_writer.hpp
+++ src/core/arch/x86_64/code_writer.hpp
@@ -11,28 +11,40 @@ public:
   using ::CodeWriter::CodeWriter;
 
   void write_return() override;
-  void write_swap(Reg, Reg) override;
   void write_neg(Reg) override;
-  void write_assign(Reg dst, Reg src) override;
+  void write_swap(Reg, Reg) override;
+
   void write_assign(Reg, i32) override;
   void write_assign(Reg, i64) override;
-  void write_assign(Reg, Mem64, i8 disp) override;
+  void write_assign(Reg dst, Reg src) override;
+  void write_assign(Reg, Mem64, i8) override;
   void write_assign(Reg, DataReg) override;
-  void write_add(Reg, i32) override;
+
   void write_add(Reg, i8) override;
+  void write_add(Reg, i32) override;
   void write_add(Reg, Reg) override;
   void write_add(Reg, Mem64) override;
+
   void write_sub(Reg, i8) override;
+
   void write_mul(IntReg, i8) override;
+
   void write_div(IntReg, IntReg) override;
+
   void write_mod(IntReg, IntReg) override;
-  void write_bit_and(Reg, Reg) override;
-  void write_bit_or(Reg, Reg) override;
-  void write_shift_left(UintReg, u8) override;
-  void write_shift_left(IntReg, u8) override;
-  void write_shift_right(UintReg, u8) override;
-  void write_shift_right(IntReg, u8) override;
+
+  void write_bit_and(Reg dst, Reg src) override;
+
+  void write_bit_or(Reg dst, Reg src) override;
+
+  void write_shift_left(UintReg, u8 count) override;
+  void write_shift_left(IntReg, u8 count) override;
+
+  void write_shift_right(UintReg, u8 count) override;
+  void write_shift_right(IntReg, u8 count) override;
+
   void write_while_neq(Reg, i8) override;
+
   void write_if_eq(Reg, i8) override;
   void write_if_else_eq(Reg, i8) override;
 };
--- src/core/codegen/code_writer.hpp
+++ src/core/codegen/code_writer.hpp
@@ -17,29 +17,40 @@ public:
   Buf get_buf() const noexcept;
 
   virtual void write_return();
-  virtual void write_swap(Reg, Reg);
   virtual void write_neg(Reg);
-  virtual void write_assign(Reg dst, Reg src);
+  virtual void write_swap(Reg, Reg);
+
   virtual void write_assign(Reg, i32);
   virtual void write_assign(Reg, i64);
+  virtual void write_assign(Reg dst, Reg src);
   virtual void write_assign(Reg, Mem64, i8);
   virtual void write_assign(Reg, DataReg);
-  virtual void write_add(Reg, i32);
+
   virtual void write_add(Reg, i8);
+  virtual void write_add(Reg, i32);
   virtual void write_add(Reg, Reg);
   virtual void write_add(Reg, Mem64);
+
   virtual void write_sub(Reg, i8);
+
   virtual void write_mul(IntReg, i8);
+
   virtual void write_div(IntReg, IntReg);
+
   virtual void write_mod(IntReg, IntReg);
-  virtual void write_bit_and(Reg, Reg);
-  virtual void write_bit_or(Reg, Reg);
-  virtual void write_shift_left(UintReg, u8);
-  virtual void write_shift_left(IntReg, u8);
-  virtual void write_shift_right(UintReg, u8);
-  virtual void write_shift_right(IntReg, u8);
+
+  virtual void write_bit_and(Reg dst, Reg src);
+
+  virtual void write_bit_or(Reg dst, Reg src);
+
+  virtual void write_shift_left(UintReg, u8 count);
+  virtual void write_shift_left(IntReg, u8 count);
+
+  virtual void write_shift_right(UintReg, u8 count);
+  virtual void write_shift_right(IntReg, u8 count);
 
   virtual void write_while_neq(Reg, i8);
+
   virtual void write_if_eq(Reg, i8);
   virtual void write_if_else_eq(Reg, i8);
 
