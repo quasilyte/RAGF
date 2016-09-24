16f05bfe6f6c6166cb461a3850e71d3b268c45fa
quasilyte <quasilyte@gmail.com>
2016-09-24 17:24:59 +0300

    run now accepts data pointer

--- src/c_api/run.h
+++ src/c_api/run.h
@@ -8,15 +8,15 @@ extern "C" {
 
 #define byte uint8_t
 
-int8_t run_int8(const byte*);
-int16_t run_int16(const byte*);
-int32_t run_int32(const byte*);
-int64_t run_int64(const byte*);
+int8_t run_int8(void* data, const byte* code);
+int16_t run_int16(void* data, const byte* code);
+int32_t run_int32(void* data, const byte* code);
+int64_t run_int64(void* data, const byte* code);
 
-uint8_t run_uint8(const byte*);
-uint16_t run_uint16(const byte*);
-uint32_t run_uint32(const byte*);
-uint64_t run_uint64(const byte*);
+uint8_t run_uint8(void* data, const byte* code);
+uint16_t run_uint16(void* data, const byte* code);
+uint32_t run_uint32(void* data, const byte* code);
+uint64_t run_uint64(void* data, const byte* code);
 
 #undef byte
 
