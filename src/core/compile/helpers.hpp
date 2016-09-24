0dbf78980d68ed2e54e80223147fce11af220744
quasilyte <quasilyte@gmail.com>
2016-09-24 18:52:38 +0300

    T* macro

--- src/core/compile/helpers.hpp
+++ src/core/compile/helpers.hpp
@@ -17,3 +17,12 @@ template<class... ARGS>
 constexpr uint label(ARGS... tokens) {
   return priv::label(0, tokens...);
 }
+
+#define T1(A) \
+  Token::A
+#define T2(A, B) \
+  label(Token::A, Token::B)
+#define T3(A, B, C) \
+  label(Token::A, Token::B, Token::C)
+#define T4(A, B, C, D) \
+  label(Token::A, Token::B, Token::C, Token::D)
